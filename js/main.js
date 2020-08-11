3fb41c675dfb4575f8663aea2d67030b65a64c65
esmolyakov <smolyakov@tecman.ru>
2020-08-11 18:09:16 +0300

    catalog

--- js/main.js
+++ js/main.js
@@ -1,29 +1,95 @@
 $(function () {
-  $('.banner-section__slider').slick({
-    dots: true,
-    prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="images/icons/arrow-left.svg"></button>',
-    nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/icons/arrow-right.svg"></button>',
-  })
+	$('.banner-section__slider').slick({
+		dots: true,
+		prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="images/icons/arrow-left.svg"></button>',
+		nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/icons/arrow-right.svg"></button>',
+	})
 
-  $('.tab').on('click', function (e) {
-    e.preventDefault();
+	$('.tab').on('click', function (e) {
+		e.preventDefault();
 
-    $('.tab').removeClass('tab--active');
-    $('.tabs-content').removeClass('tabs-content--active');
+		$($(this).siblings()).removeClass('tab--active');
+		$($(this).parent().siblings().find('div')).removeClass('tabs-content--active');
 
-    $(this).addClass('tab--active');
-    $($(this).attr('href')).addClass('tabs-content--active')
-  });
+		$(this).addClass('tab--active');
+		$($(this).attr('href')).addClass('tabs-content--active')
+	});
 
-  $('.product-item__favorite').on('click', function () {
-    $(this).toggleClass('product-item__favorite--active')
-  });
+	$('.product-item__favorite').on('click', function () {
+		$(this).toggleClass('product-item__favorite--active')
+	});
 
-  $('.product-slider').slick({
-    slidesToShow: 4,
-    sldesToScroll: 1,
-    prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="images/icons/arrow-black-left.svg"></button>',
-    nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="images/icons/arrow-black-right.svg"></button>',
-  })
+	$('.product-slider').slick({
+		slidesToShow: 4,
+		sldesToScroll: 1,
+		prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="images/icons/arrow-black-left.svg"></button>',
+		nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="images/icons/arrow-black-right.svg"></button>',
+	})
+
+	$('.filter__item-drop').on('click', function () {
+		$(this).toggleClass('filter__item-drop--active')
+		$(this).next().slideToggle('200')
+	})
+
+	$('.filter-stile').styler();
+
+	var $range = $(".js-range-slider"),
+		$inputFrom = $(".js-input-from"),
+		$inputTo = $(".js-input-to"),
+		instance,
+		min = 0,
+		max = 1000,
+		from = 0,
+		to = 0;
+
+	$range.ionRangeSlider({
+		skin: "round",
+		type: "double",
+		min: min,
+		max: max,
+		from: 200,
+		to: 800,
+		onStart: updateInputs,
+		onChange: updateInputs
+	});
+	instance = $range.data("ionRangeSlider");
+
+	function updateInputs(data) {
+		from = data.from;
+		to = data.to;
+
+		$inputFrom.prop("value", from);
+		$inputTo.prop("value", to);
+	}
+
+	$inputFrom.on("input", function () {
+		var val = $(this).prop("value");
+
+		// validate
+		if (val < min) {
+			val = min;
+		} else if (val > to) {
+			val = to;
+		}
+
+		instance.update({
+			from: val
+		});
+	});
+
+	$inputTo.on("input", function () {
+		var val = $(this).prop("value");
+
+		// validate
+		if (val < from) {
+			val = from;
+		} else if (val > max) {
+			val = max;
+		}
+
+		instance.update({
+			to: val
+		});
+	});
 
 });
